e6305971ca2e9f5bbeea37390c84f1e5e5120f33
H4XOR_ <hakroot07@gmail.com>
2024-08-04 01:06:00 +0200

    create var and flag: mode and secret key

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "flag"
+
+var cipherMode = flag.Bool("cipher", true, "Enable cipher mode. This is the default mode.")
+var decipherMode = flag.Bool("decipher", false, "Enable decipher mode.")
+var secretKey = flag.String("secret", "", "Your secret key. Must contain at least 1 character")
+
 func main() {
 
 }
