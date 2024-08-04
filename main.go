49666ff799a86a7af42563739bcff36dc25ceb88
H4XOR_ <hakroot07@gmail.com>
2024-08-04 22:19:38 +0200

    Add function 'getUserInput' and changed the flag check

--- main.go
+++ main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-var cipherMode = flag.Bool("cipher", true, "Enable cipher mode. This is the default mode.")
-var decipherMode = flag.Bool("decipher", false, "Enable decipher mode.")
+var mode = flag.String("mode", "cipher", "Set to 'cipher' or 'decipher'. Default is 'cipher'.")
 var secretKey = flag.String("secret", "", "Your secret key. Must contain at least 1 character")
 
 func main() {
 	flag.Parse()
 
-	if *cipherMode && *decipherMode {
-		fmt.Println("Please specify only one mode at a time.")
+	switch *mode {
+	case "cipher":
+		plaintext := getUserInput("Enter your text to cipher: ")
+		fmt.Println(plaintext)
+	case "decipher":
+		cipheredText := getUserInput("Enter your ciphered data to decipher: ")
+	default:
+		fmt.Println("Invalid mode. Use 'cipher' or 'decipher'.")
 		os.Exit(1)
 	}
 
 	if len(*secretKey) == 0 {
 		fmt.Println("No secret is provided! Exiting now ...")
 		os.Exit(1)
+	}
+}
+
+func getUserInput(msg string) string {
+	fmt.Print(msg)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		result, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading the entered text! Please try again")
+			continue
+		}
+
+		return strings.TrimRight(result, "\n")
 	}
 }
