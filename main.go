d99337ea0443c4c1eb4a769be29314e30de10cae
H4XOR_ <hakroot07@gmail.com>
2024-08-04 21:42:49 +0200

    flag check

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 var cipherMode = flag.Bool("cipher", true, "Enable cipher mode. This is the default mode.")
 var decipherMode = flag.Bool("decipher", false, "Enable decipher mode.")
 var secretKey = flag.String("secret", "", "Your secret key. Must contain at least 1 character")
 
 func main() {
+	flag.Parse()
 
+	if *cipherMode && *decipherMode {
+		fmt.Println("Please specify only one mode at a time.")
+		os.Exit(1)
+	}
+
+	if len(*secretKey) == 0 {
+		fmt.Println("No secret is provided! Exiting now ...")
+		os.Exit(1)
+	}
 }
